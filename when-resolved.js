4e4fb5bf16ae6828b6a2a99e7b4d37c334340a36
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-07-14 07:25:12 -0400

    Initial commit

--- when-resolved.js
+++ when-resolved.js
@@ -14,6 +14,34 @@ class WhenResolved extends BE {
      * @type {BEConfig<AP & BEAllProps, Actions & IEnhancement>}
      */
     static config = {
+        propInfo: {
+            ...propInfo,
+            parsedStatements: {},
+            rawStatements: {},
+        },
+        positractions: [resolved, rejected],
+        compacts:{
+            when_parsedStatements_changes_call_hydrate: 0,
+        }
+    }
+
+    de = de;
+
+    /**
+     * 
+     * @param {BAP} self 
+     * @returns 
+     */
+    async hydrate(self){
+        const {parsedStatements} = self;
+        const {find} = await import('trans-render/dss/find.js');
+        const {whenResolved} = await import('be-hive/whenResolved.js');
+        for(const parsedStatement of parsedStatements){
+            const {localSpecifier, remoteSpecifier} = parsedStatement;
+        }
+        return /** @type {PAP} */({
+            resolved: true,
+        })
     }
 }
 
