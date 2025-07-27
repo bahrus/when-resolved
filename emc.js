19bd9a1db4a2749211cc625306213d7939ea3e13
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-07-27 13:44:01 -0400

    0.0.1

--- emc.js
+++ emc.js
@@ -3,7 +3,10 @@ import { BeHive, MountObserver, seed } from 'be-hive/be-hive.js';
 /** @import {AP} from './ts-refs/when-resolved/types'; */
 /** @import {EMC} from './ts-refs/trans-render/be/types' */
 
-const remoteSpecifierStringSetLocalProp = String.raw `^(?<remoteSpecifierString>.*) set (?<localSpecifierString>.*)`;
+const remoteSpecifierStringSetLocalProp = 
+    String.raw `^(?<remoteSpecifierString>.*) set (?<localSpecifierString>.*)`;
+
+const remoteSpecifierStringSetLocalPropToRemoteProp = String.raw `${remoteSpecifierStringSetLocalProp} to (?<remotePropertyPath>.*)`;
 
 /**
  * @type {[string, string]}
@@ -25,6 +28,11 @@ export const emc = {
             objValMapsTo: '.',
             regExpExts: {
                 parsedStatements: [
+                    {
+                        regExp: remoteSpecifierStringSetLocalPropToRemoteProp,
+                        defaultVals: {},
+                        dssKeys: [['remoteSpecifierString', 'remoteSpecifier'], ['localSpecifierString', 'localSpecifier']],
+                    },
                     {
                         regExp: remoteSpecifierStringSetLocalProp,
                         defaultVals: {},
